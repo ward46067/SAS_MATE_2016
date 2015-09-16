47e4ea97c9574c4a6dff012625e69405d9e66822
ward46067 <ward46067@sas.edu.sg>
2015-09-17 07:56:39 +0800

    equations

--- SAS MATE Equations Test/src/main.java
+++ SAS MATE Equations Test/src/main.java
@@ -43,13 +43,6 @@ public class main {
         console.scan();
         y = Double.parseDouble(console.usrAnswer);
         
-        if(y == 0) {
-            servo1 = 90;
-            servo2 = 90;
-            servo3 = 90;
-            servo4 = 90;
-        }
-
         if(x == 0) {
           motor1x = 0.0;
           motor2x = 0;
@@ -62,6 +55,14 @@ public class main {
           motor2z = 0;
           motor3z = 0;
           motor4z = 0;
+        }
+        
+        
+        if(y == 0) {
+            servo1 = 90;
+            servo2 = 90;
+            servo3 = 90;
+            servo4 = 90;
         }
 
         if(x < 0) {
@@ -93,32 +94,30 @@ public class main {
           motor4z = z;
         }
 
-        motor1 = motor1x + (motor1z);
+        motor1 = motor1x + motor1z;
         motor2 = motor2x + motor2z;
         motor3 = motor3x + motor3z;
         motor4 = motor4x + motor4z;
         
         if(y > 0) {
 
-            if(motor1 > 0){
-                //servo1 = ((motor1 / 2) + y) * 90;
-                servo1 = 1.0;
-            }
+            if(motor1 > 0)
+                servo1 = ((motor1 / 2) + y) * 90; 
             if(motor2 > 0)
-                servo2 = ((motor2 + y)/2) * 90;
+                servo2 = ((motor2 / 2) + y) * 90;
             if(motor3 > 0)
-                servo3 = ((motor3 + y)/2) * 90;
+                servo3 = ((motor3 / 2) + y) * 90 - 90;
             if(motor4 > 0)
-                servo4 = ((motor4 + y)/2) * 90;
+                servo4 = ((motor4 / 2) + y) * 90 - 90;
 
             if(motor1 < 0)
-                servo1 = (((-motor1 + y)/2) * 90) + 90;
+                servo1 = ((-motor1 / 2) + y) * 90 - 90;
             if(motor2 < 0)
-                servo2 = (((-motor2 + y)/2) * 90) + 90;
+                servo2 = ((-motor2 / 2) + y) * 90 - 90;
             if(motor3 < 0)
-                servo3 = (((-motor3 + y)/2) * 90) + 90;
+                servo3 = ((-motor3 / 2) + y) * 90;
             if(motor4 < 0)
-                servo4 = (((-motor4 + y)/2) * 90) + 90;
+                servo4 = ((-motor4 / 2) + y) * 90;
 
             if(motor1 == 0){
                 //servo1 = 90 - (y *90);
