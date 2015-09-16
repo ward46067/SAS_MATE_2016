b664133ae5a0cce744c544910eae25288db05b37
ward46067 <ward46067@sas.edu.sg>
2015-09-16 21:18:45 +0800

    added motor speed for y

--- SAS MATE Equations Test/src/main.java
+++ SAS MATE Equations Test/src/main.java
@@ -116,14 +116,22 @@ public class main {
               if(motor4 < 0)
                   servo4 = (((-motor4 + y)/2) * 90) + 90;
               
-              if(motor1 == 0)
+              if(motor1 == 0){
                   servo1 = y *90;
-              if(motor2 == 0)
+                  motor1 = y;
+              }
+              if(motor2 == 0){
                   servo2 = y *90;
-              if(motor3 == 0)
+                  motor2 = y;
+              }
+              if(motor3 == 0){
                   servo3 = y *90;
-              if(motor4 == 0)
+                  motor3 = y;
+              }
+              if(motor4 == 0){
                   servo4 = y *90;
+                  motor4 = y;
+              }
           }
           
           if(y < 1) {
@@ -146,14 +154,22 @@ public class main {
               if(motor4 < 0)
                   servo4 = ((-motor4 + y)/2) * 90;
               
-              if(motor1 == 0)
+              if(motor1 == 0){
                   servo1 = (y * 90) + 90;
-              if(motor2 == 0)
+                  motor1 = -y;
+              }
+              if(motor2 == 0){
                   servo2 = (y * 90) + 90;
-              if(motor3 == 0)
+                  motor2 = -y;
+              }
+              if(motor3 == 0){
                   servo3 = (y * 90) + 90;
-              if(motor4 == 0)
+                  motor3 = -y;
+              }
+              if(motor4 == 0){
                   servo4 = (y * 90) + 90;
+                  motor4 = -y;
+              }
             
           }
           
@@ -164,5 +180,6 @@ public class main {
           System.out.println("3, " + servo3 + ", " + motor3 + ".");
           System.out.println("4, " + servo4 + ", " + motor4 + ".");
     }
+}    
+
     
-}
